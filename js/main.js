aa1b1b18957f208bf89a676b87202a41bd15e0e4
MattijnStolk <mattijn001@gmail.com>
2021-11-27 21:14:26 +0100

    cleanup code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-//to start db run "C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe"
+//to start db on windows run "C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe"
 //or on linux "sudo systemctl start mongod"
 const express = require('express')
 const env = require('dotenv')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const notesRouter = require('../routes/notesRoutes.js')();
 
 mongoose.connect('mongodb://localhost/notes')
 app = express()
@@ -13,12 +14,11 @@ console.log('starting up webservice')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extendedparser: true, extended:true } ))
 
-let notesRouter = require('../routes/notesRoutes.js')();
-
 app.get('/', function (req, res) {
     res.header("Content-Type", "application/json")
     res.send('{"message": "hello world"}')
   })
 
 app.use('/api', notesRouter)
+
 app.listen(8000)
